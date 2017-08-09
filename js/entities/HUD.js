28fbf429704cb6f447799a1da70ce71cb6a46c2b
Justin Soon <justin.soon@verizon.net>
2017-08-09 08:40:29 -0400

    Redacted

---
+++ js/entities/HUD.js
@@ -0,0 +1,123 @@
+game.HUD = game.HUD || {};
+
+game.HUD.Container = me.Container.extend({
+    init: function() {
+        // call the constructor
+        this._super(me.Container, 'init');
+        // persistent across level change
+        this.isPersistent = true;
+
+        // non collidable
+        this.collidable = false;
+
+        // make sure our object is always draw first
+        this.z = Infinity;
+
+        // give a name
+        this.name = "HUD";
+
+        // add our child score object at the top left corner
+        this.addChild(new game.HUD.ScoreItem(5, 5));
+    }
+});
+
+
+/**
+ * a basic HUD item to display score
+ */
+game.HUD.ScoreItem = me.Renderable.extend({
+    /**
+     * constructor
+     */
+    init: function(x, y) {
+        // call the parent constructor
+        // (size does not matter here)
+        this._super(me.Renderable, "init", [x, y, 10, 10]);
+
+        // local copy of the global score
+        this.stepsFont = new me.Font('gamefont', 80, '#000', 'center');
+
+        // make sure we use screen coordinates
+        this.floating = true;
+    },
+
+    draw: function (renderer) {
+        if (game.data.start && me.state.isCurrent(me.state.PLAY))
+            this.stepsFont.draw(renderer, game.data.steps, me.video.renderer.getWidth()/2, 10);
+    }
+
+});
+
+var BackgroundLayer = me.ImageLayer.extend({
+    init: function(image, z, speed) {
+        var settings = {};
+        settings.name = image;
+        settings.width = 900;
+        settings.height = 600;
+        settings.image = image;
+        settings.z = z;
+        settings.ratio = 1;
+        // call parent constructor
+        this._super(me.ImageLayer, 'init', [0, 0, settings]);
+    },
+
+    update: function() {
+        if (me.input.isKeyPressed('mute')) {
+            game.data.muted = !game.data.muted;
+            if (game.data.muted){
+                me.audio.disable();
+            }else{
+                me.audio.enable();
+            }
+        }
+        return true;
+    }
+});
+
+var Share = me.GUI_Object.extend({
+    init: function(x, y) {
+        var settings = {};
+        settings.image = "share";
+        settings.framewidth = 150;
+        settings.frameheight = 75;
+        this._super(me.GUI_Object, 'init', [x, y, settings]);
+    },
+
+    onClick: function(event) {
+        var shareText = 'Just made ' + game.data.steps + ' steps on buskey Bird! Can you beat me? Try online here!';
+        var url = 'https://justinsoon.github.io/';
+        FB.ui(
+            {
+             method: 'feed',
+             name: 'My buskey Bird Score!',
+             caption: "Share to your friends",
+             description: (
+                    shareText
+             ),
+             link: url,
+             picture: 'https://raw.githubusercontent.com/justinsoon/justinsoon.github.io/master/data/img/clumsy.png'
+            }
+        );
+        return false;
+    }
+
+});
+
+var Tweet = me.GUI_Object.extend({
+    init: function(x, y) {
+        var settings = {};
+        settings.image = "tweet";
+        settings.framewidth = 152;
+        settings.frameheight = 75;
+        this._super(me.GUI_Object, 'init', [x, y, settings]);
+    },
+
+    onClick: function(event) {
+        var shareText = 'Just made ' + game.data.steps + ' steps on buskey Bird! Can you beat me? Try online here!';
+        var url = 'http://justinsoon.github.io/';
+        var hashtags = 'buskeybird,melonjs'
+        window.open('https://twitter.com/intent/tweet?text=' + shareText + '&hashtags=' + hashtags + '&count=' + url + '&url=' + url, 'Tweet!', 'height=300,width=400')
+        return false;
+    }
+
+});
